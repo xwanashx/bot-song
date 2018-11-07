0e0bebcad162e4f77706102190823db207743707
xwanashx <44838264+xwanashx@users.noreply.github.com>
2018-11-07 16:24:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -316,7 +316,7 @@ client.on('message', message => {
     if (message.content === 'help') {
         let helpEmbed = new Discord.RichEmbed()
         .setTitle('**أوامر الميوزك...**')
-        .setDescription('**برفكس البوت (!)**')
+        .setDescription('**برفكس البوت (MA)**')
         .addField('play', 'لتشغيل اغنية')
         .addField('join', 'دخول رومك الصوتي')
         .addField('disconnect', 'الخروج من رومك الصوتي')
